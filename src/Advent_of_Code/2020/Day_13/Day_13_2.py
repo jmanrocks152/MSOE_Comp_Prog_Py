0cce5cf086bc5567510a1c1a92e3320493f62695
jmanrocks152 <jmanrocks152@gmail.com>
2021-03-03 10:25:13 -0600

    Completed Day 13 part 2

--- src/Advent_of_Code/2020/Day_13/Day_13_2.py
+++ src/Advent_of_Code/2020/Day_13/Day_13_2.py
@@ -1,5 +1,5 @@
 import os
-import math
+from functools import reduce
 
 file_path = os.path.join(os.path.dirname(__file__), "input.txt")
 
@@ -15,6 +15,42 @@ buses = [int(s) for s in buses]
 
 for i in range(len(buses)):
     current_bus = buses[i]
-    buses[i] = current_bus, split_buses.index(current_bus)
+    buses[i] = current_bus, split_buses.index(str(current_bus))
 
-min_timestamp = math.ceil(100000000000000 / buses[0][0]) * buses[0][0]
+
+# Python 3.6
+# Source: Rosetta Code
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a * b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
+
+
+m = [[] for i in range(len(buses))]
+
+for i in range(len(m)):
+    m[i] = buses[i][0]
+
+x = [[] for i in range(len(buses))]
+
+for i in range(len(x)):
+    x[i] = buses[i][0] - buses[i][1]
+
+# t % bus_id == 0
+# t + 1 % bus_id == 0 (this is the same as) t % bus_id - 1 == 0
+print(chinese_remainder(m, x))
