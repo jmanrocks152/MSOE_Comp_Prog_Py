c4830c93b1eff827fca969bcdb29f3a2a73e10ee
jmanrocks152 <jmanrocks152@gmail.com>
2021-03-05 21:24:12 -0600

    Began work on Day 15 part 1, completed day 16 part 1

--- src/Advent_of_Code/2020/Day_15/Day_15_2.py
+++ src/Advent_of_Code/2020/Day_15/Day_15_2.py
@@ -8,35 +8,24 @@ with open(file_path, 'r') as input:
 starting_numbers = all_lines[0].split(",")
 starting_numbers = [int(s) for s in starting_numbers]
 
-spoken_numbers = []
+spoken_numbers = {}
 
-for number in starting_numbers:
-    spoken_numbers.append(number)
+for i in range(len(starting_numbers)):
+    spoken_numbers[starting_numbers[i]] = i
 
-spoken_numbers.append(0)
+current_number = 0
+spoken_numbers[current_number] = len(starting_numbers)
+# TODO: Debug logic
+for i in range(len(spoken_numbers) + 1, 30000000 + 1):
+    current_number_index = spoken_numbers.get(current_number, i)
 
-
-def guess_seq_len(seq):
-    guess = 1
-    max_len = int(len(seq) / 2)
-    for x in range(2, max_len):
-        if seq[0:x] == seq[x:2*x] :
-            return x
-
-    return guess
-
-
-for i in range(len(spoken_numbers), 30000000):
-    last_number = spoken_numbers[len(spoken_numbers) - 1]
-    temp_spoken_numbers = spoken_numbers[:len(spoken_numbers) - 1]
-
-    if last_number in temp_spoken_numbers:
-        spoken_numbers.append(i - (len(temp_spoken_numbers) - temp_spoken_numbers[::-1].index(last_number)))
+    if not current_number_index == i:
+        old_current_number = current_number
+        current_number = current_number_index - current_number
+        spoken_numbers[old_current_number] = i
     else:
-        spoken_numbers.append(0)
+        spoken_numbers[current_number] = i
+        current_number = 0
 
-    seq_len = guess_seq_len(spoken_numbers)
-    if not seq_len == 1:
-        print(spoken_numbers[:seq_len - 1])
 
-print(spoken_numbers[30000000 - 1])
+print(list(spoken_numbers.keys())[list(spoken_numbers.values()).index(30000000)])
